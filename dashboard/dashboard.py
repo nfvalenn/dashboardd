3428f7da9976a37f511369f78abffecaedd1634b
Nabila Febriyanti Valentin <113290656+nfvalenn@users.noreply.github.com>
2025-03-09 23:24:17 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -50,7 +50,7 @@ def create_pengiriman_cepat(df):
     pengiriman_cepat.head(10)
 
 chunk_size = 100000
-url = "https://raw.githubusercontent.com/nfvalenn/dashboardd/main/all_data.csv.gz"
+url = "https://raw.githubusercontent.com/nfvalenn/dashboardd/dashboard/all_data.csv.gz"
 chunks = pd.read_csv(url, chunksize=chunk_size, compression="gzip")
 
 all_data = pd.concat([chunk for _, chunk in zip(range(5), chunks)])
