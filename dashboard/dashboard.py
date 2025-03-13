2747ef5e7c161a9f2cf4c575f49744410be90eea
Nabila Febriyanti Valentin <vanabila634@gmail.com>
2025-03-13 13:33:12 +0700

    solve dashboard

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,9 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import folium
-from folium.plugins import HeatMap
-from streamlit_folium import folium_static
 import streamlit as st
 from babel.numbers import format_currency
 
@@ -37,17 +34,9 @@ def create_sum_order_items_df(df):
     sum_order_items_df.columns = ["product_name", "sales_count"]
     return sum_order_items_df
 
-def create_mean_city_delivery_time(df):
-    mean_city_delivery_time = df.groupby('customer_city')['delivery_time'].mean().reset_index()
-    return mean_city_delivery_time
-
-def create_pengiriman_lambat(df):
-    pengiriman_lambat = mean_city_delivery_time.nlargest(10, 'delivery_time')
-    pengiriman_lambat.head(10)
-
-def create_pengiriman_cepat(df):
-    pengiriman_cepat = mean_city_delivery_time.nsmallest(10, 'delivery_time')
-    pengiriman_cepat.head(10)
+def create_order_payments_df(df):
+    order_payments_df = df.groupby("payment_type").order_id.nunique().reset_index()
+    return order_payments_df
 
 chunk_size = 100000
 url = "https://raw.githubusercontent.com/nfvalenn/dashboardd/main/dashboard/all_data.csv.gz"
@@ -83,9 +72,7 @@ with st.sidebar:
     top_10_city = create_top_10_city(main_df)
     top_10_state = create_top_10_state(main_df)
     sum_order_item = create_sum_order_items_df(main_df)
-    mean_city_delivery_time = create_mean_city_delivery_time(main_df)
-    pengiriman_lambat = create_pengiriman_lambat(main_df)
-    pengiriman_cepat = create_pengiriman_cepat(main_df)
+    order_payment = create_order_payments_df(main_df)
 
 st.header('E-Commarce Dashboard Data Analysis:sparkles:')
 
@@ -115,28 +102,30 @@ ax.tick_params(axis='x', labelsize=15)
 st.pyplot(fig)
 
 st.subheader("10 Kota dan Negara dengan Pelanggan Terbanyak")
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(24, 6))
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(50, 12))
 
 colors = ["#663399", "#75489D", "#865FA9", "#9775B5", "#A88CC2", "#B9A2CE", "#CBB9DA", "#D1BCD8", "#D9C4E0", "#E1CDE7"]
 
 sns.barplot(x="customer_count", y="customer_city", hue="customer_city", data=top_10_city.head(10), palette=colors, ax=ax[0])
-ax[0].set_ylabel("Jumlah Pelanggan")
-ax[0].set_xlabel("Kota")
-ax[0].set_title("10 Kota dengan Pelanggan Terbanyak", loc="center", fontsize=18)
-ax[0].tick_params(axis ='y', labelsize=15)
+ax[0].set_ylabel("Kota", fontsize = 40)
+ax[0].set_xlabel("Jumlah Pelanggan", fontsize = 40)
+ax[0].set_title("10 Kota dengan Pelanggan Terbanyak", loc="center", fontsize=50, pad=30)
+ax[0].tick_params(axis ='y', labelsize=35)
+ax[0].tick_params(axis='x', labelsize=35)
 
 sns.barplot(x="customer_count", y="customer_state", hue="customer_state", data=top_10_state.head(10), palette=colors, ax=ax[1])
-ax[1].set_ylabel("Jumlah Pelanggan")
-ax[1].set_xlabel("Negara")
+ax[1].set_ylabel("Negara", fontsize = 40)
+ax[1].set_xlabel("Jumlah Pelanggan", fontsize = 40)
 ax[1].invert_xaxis()
 ax[1].yaxis.set_label_position("right")
 ax[1].yaxis.tick_right()
-ax[1].set_title("10 Negara dengan Pelanggan Terbanyak", loc="center", fontsize=18)
-ax[1].tick_params(axis='y', labelsize=15)
+ax[1].set_title("10 Negara dengan Pelanggan Terbanyak", loc="center", fontsize=50, pad=30)
+ax[1].tick_params(axis='y', labelsize=35)
+ax[1].tick_params(axis='x', labelsize=35)
 
 st.pyplot(fig)
 
-st.subheader("Best Performing Product")
+st.subheader("Penjualan Jenis Produk tertinggi")
 fig, ax = plt.subplots(figsize=(10, 5))
 
 colors_red = ["#993333", "#A54444", "#B25555", "#C06666", "#CD7777", "#DA8888", "#E79999", "#F5AAAA", "#FFBBBB", "#FFCCCC"]
@@ -148,28 +137,19 @@ sns.barplot(
     data=sum_order_item.sort_values(by="sales_count", ascending=False).head(10),
     palette=colors_red
 )
-ax.set_title("Penjualan Jenis Produk tertinggi", loc="center", fontsize=15)
 ax.set_ylabel(None)
 ax.set_xlabel(None)
 
 ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha="right", fontsize=12)
 st.pyplot(fig)
 
-st.subheader("Pengiriman Paling Cepat dan Paling Lambat")
+st.subheader("Metode Pembayaran Yang Paling Sering Digunakan")
+plt.figure(figsize=(10, 7))
+palette_color = sns.color_palette('pink') 
 
-st.write("""
-**Keterangan:**  
-Peta ini menampilkan **analisis waktu pengiriman** berdasarkan lokasi menggunakan **Heatmap**.  
-- **Merah** → Pengiriman lebih lama  
-- **Hijau/Biru** → Pengiriman lebih cepat  
-""")
+plt.pie(order_payment["order_id"], labels=order_payment["payment_type"], colors=palette_color, autopct='%.0f%%', pctdistance=0.85)
+plt.tight_layout()
 
-m = folium.Map(location=(-14.242915, -53.189266), zoom_start=5, width=500, height=350)
-geo_delivery_clean = all_data.dropna(subset=['geolocation_lat', 'geolocation_lng', 'delivery_time'])
-
-heat_data = list(zip(geo_delivery_clean['geolocation_lat'], geo_delivery_clean['geolocation_lng'], geo_delivery_clean['delivery_time']))
-
-HeatMap(heat_data).add_to(m)
-folium_static(m)
+st.pyplot(plt)
 
 st.caption('Copyright © Nabila Febriyanti Valentin 2025')
